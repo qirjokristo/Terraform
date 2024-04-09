e8437c36ea95d9cd19c55cd9c1058202e2fdca2b
qirjokristo <kristoqijoal@gmail.com>
2024-04-09 16:34:15 +0200

    removed access and secret key

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
 provider "aws" {
-
-  ##Temporary access keys for sandbox
-  #  access_key = "AKIA5FTZBQ3OTX7BWQP6"
-  #  secret_key = "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"
   region = "us-east-1"
 }
 
