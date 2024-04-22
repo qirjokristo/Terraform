e682647acb79ffa9a1c448bb0766ee72569cf00b
qirjokristo <kristoqijoal@gmail.com>
2024-04-23 00:06:43 +0200

    data achieved

--- locals.tf
+++ locals.tf
@@ -1,5 +1,4 @@
 locals {
-  hosted_zones = jsondecode(file("${path.module}/templates/zones.tpl"))
-  hosted_name_json = jsonencode(local.hosted_zones.HostedZones) 
-  hosted_name = jsondecode(local.hosted_name_json)
+  json1 = substr("${file("${path.module}/templates/zones.tpl")}",25,378)
+  zone = jsondecode(local.json1)
 }
