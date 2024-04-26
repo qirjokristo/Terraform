b74ed787406d1ac1c59ec48587b858d2caed472a
qirjokristo <kristoqijoal@gmail.com>
2024-04-26 16:21:44 +0200

    lambda more dev

--- lambda.tf
+++ lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "cleanup" {
   function_name = "kristo_cleanup"
-  role = aws_iam_role.lambda.arn
-  runtime = "python3.8"
-  handler = "lambda_function.lambda_handler"
-  
+  role          = aws_iam_role.lambda.arn
+  runtime       = "python3.8"
+  handler       = "lambda_function.lambda_handler"
+
 }
