0e64f65f287eff4be2b0c0ce81ba15cb7fd17629
qirjokristo <kristoqijoal@gmail.com>
2024-05-14 10:58:19 +0200

    eks-dev

--- eks.tf
+++ eks.tf
@@ -8,3 +8,15 @@ resource "aws_eks_cluster" "kristo" {
     endpoint_private_access = true
   }
 }
+
+resource "aws_eks_node_group" "kristo" {
+  cluster_name = aws_eks_cluster.kristo.name
+  subnet_ids = aws_subnet.pub[*].id
+  node_role_arn = aws_iam_role.eks.arn
+  node_group_name = "kristo-test-ng"
+  scaling_config {
+    desired_size = 3
+    max_size = 6
+    min_size = 2
+  }
+}
