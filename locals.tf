c07c90622274dc00674abb8763175a58252526b5
qirjokristo <kristoqijoal@gmail.com>
2024-06-14 11:45:02 +0200

    dns fix

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  json1     = substr(file("${path.module}/templates/zones.tpl"), 25, 378)
+  json1     = substr(file("${path.module}/templates/zones.tpl"), 25, 369)
   zone      = jsondecode(local.json1)
   apex_zone = trimsuffix(local.zone.Name, ".")
   website   = "https://${data.aws_route53_zone.website.name}"
