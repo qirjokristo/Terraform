b565ff6692be654abd05a19a95f33d14449dc3b3
qirjokristo <kristoqijoal@gmail.com>
2024-07-27 00:24:02 +0200

    ebs driver fix

--- eks.tf
+++ eks.tf
@@ -20,14 +20,8 @@ resource "aws_eks_addon" "eks-pod-identity-agent" {
   addon_name   = "eks-pod-identity-agent"
 }
 
-resource "aws_eks_addon" "aws-ebs-csi-driver" {
-  cluster_name = aws_eks_cluster.kristo.name
-  addon_name   = "aws-ebs-csi-driver"
-}
-
-
 resource "aws_eks_node_group" "kristo" {
-  depends_on      = [aws_eks_addon.vpc-cni, aws_eks_addon.eks-pod-identity-agent, aws_eks_addon.aws-ebs-csi-driver]
+  depends_on      = [aws_eks_addon.vpc-cni, aws_eks_addon.eks-pod-identity-agent]
   cluster_name    = aws_eks_cluster.kristo.name
   subnet_ids      = aws_subnet.pub[*].id
   node_role_arn   = aws_iam_role.eksworker.arn
@@ -38,4 +32,10 @@ resource "aws_eks_node_group" "kristo" {
     max_size     = 6
     min_size     = 2
   }
+}
+
+resource "aws_eks_addon" "aws-ebs-csi-driver" {
+  depends_on = [ aws_eks_node_group.kristo ]
+  cluster_name = aws_eks_cluster.kristo.name
+  addon_name   = "aws-ebs-csi-driver"
 }
