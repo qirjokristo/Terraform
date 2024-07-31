6c9dc713f635ad122c107305489480b145993f06
qirjokristo <kristoqijoal@gmail.com>
2024-07-31 12:18:05 +0200

    state fix

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+# terraform {
+#   backend "s3" {
+#     bucket = aws_s3_bucket.kristo.name
+#     key    = "state/"
+#     region = "us-east-1"
+#   }
+# }
+
 provider "aws" {
   region = "us-east-1"
 }
