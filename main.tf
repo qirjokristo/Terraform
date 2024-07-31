59770db1dd8ffee97ce486d844d6e2abb31a9f6f
qirjokristo <kristoqijoal@gmail.com>
2024-07-31 15:16:55 +0200

    s3 states

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+# terraform {
+#   backend "s3" {
+#     bucket = "kristo-tfstates-UPDATE"
+#     key = "/state/master.tfstate"
+#     region = "us-east-1"
+#   }
+# }
+
+
 provider "aws" {
   region = "us-east-1"
 }
